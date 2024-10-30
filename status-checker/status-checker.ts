15b936c816c33bf5fe1e1314b17600da4facb874
Aritra Mondal <aritra041999.dev@gmail.com>
2024-10-30 20:02:50 +0530

    feat: replace status checking to worker

--- status-checker/status-checker.ts
+++ status-checker/status-checker.ts
@@ -1,10 +1,7 @@
 import { db } from "./db";
-import { Website, WebsiteResponse } from "./types";
-import { getStatus } from "./utils";
-import { logger } from "@trigger.dev/sdk/v3";
+import { Website } from "./types";
 
-const BATCH_SIZE = 20;
-const TIMEOUT = 5000;
+const BATCH_SIZE = 5;
 
 export const fetchWebsites = async (interval: number): Promise<Website[]> => {
   return await db`
@@ -22,82 +19,39 @@ export const fetchWebsites = async (interval: number): Promise<Website[]> => {
     })
     .catch((error) => {
       console.error(`Error fetching websites ${error.message}`);
-      logger.error(`Error fetching websites ${error.message}`);
       throw error;
     });
 };
 
-export const insertStatus = async (
-  uptimeResponses: WebsiteResponse[],
-): Promise<void> => {
-  const values = uptimeResponses.map((response) => {
-    return {
-      website_id: response.id,
-      status: response.status,
-      response_time: response.responseTime,
-      status_code: response.statusCode,
-    };
-  });
-
-  await db`insert into uptime_checks ${db(values, "website_id", "status", "response_time", "status_code")}`.catch(
-    (error) => {
-      console.error(`Error inserting statuses ${error.message}`);
-      logger.error(`Error inserting statuses ${error.message}`);
-    },
-  );
-};
-
-export const pingWebsites = async (interval: number): Promise<void> => {
+export const invokeWorker = async (interval: number): Promise<void> => {
   const websites = await fetchWebsites(interval);
-
+  const apiKey = process.env.API_KEY!;
   for (let i = 0; i < websites.length; i += BATCH_SIZE) {
-    const batch = websites.slice(i, i + BATCH_SIZE);
-    const batchPromises = batch.map(pingWebsite);
-    const batchResults = await Promise.all(batchPromises);
-    await insertStatus(batchResults);
+    const batch = websites.slice(i, i + BATCH_SIZE).map((website) => {
+      return {
+        websiteId: website.id,
+        url: website.url,
+      };
+    });
+    fetch("https://monitor-worker.vercel.app/api/index", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+      body: JSON.stringify({ region: "hello world", urls: batch }),
+    });
   }
 
   console.log(`Pinged ${websites.length} websites with interval ${interval}`);
-  logger.info(`Pinged ${websites.length} websites with interval ${interval}`);
-};
-
-export const pingWebsite = async (
-  website: Website,
-): Promise<WebsiteResponse> => {
-  const startTime = Date.now();
-
-  return await fetch(website.url, {
-    signal: AbortSignal.timeout(TIMEOUT),
-  })
-    .then((response) => {
-      const endTime = Date.now();
-      return {
-        ...website,
-        statusCode: response.status,
-        status: getStatus(response.status),
-        responseTime: endTime - startTime,
-      };
-    })
-    .catch((error) => {
-      console.error(
-        `Error fetching website (${website.id}, ${website.url}}): ${error.message}`,
-      );
-      logger.error(
-        `Error fetching website (${website.id}, ${website.url}}): ${error.message}`,
-      );
-      throw error;
-    });
 };
 
 export const checkStatus = async (interval: number): Promise<void> => {
   console.log(`Fetching websites for interval: ${interval}`);
-  logger.info(`Fetching websites for interval: ${interval}`);
 
-  await pingWebsites(interval).catch((error) => {
-    logger.error(error);
+  await invokeWorker(interval).catch((error) => {
     console.error(error);
   });
 
   console.log(`Finished fetching websites for interval: ${interval}`);
-  logger.info(`Finished fetching websites for interval: ${interval}`);
 };
